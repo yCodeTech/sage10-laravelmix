860d2145c3be9fd3487a0c0ddc9df0e24c06f341
yCodeTech <studocwho@hotmail.com>
2025-01-16 22:14:01 +0000

    refactor: 🎨 Move default theme markup to the app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,17 +1,38 @@
-<a class="sr-only focus:not-sr-only" href="#main">
-  {{ __('Skip to content') }}
-</a>
+<!doctype html>
+<html @php(language_attributes())>
 
-@include('sections.header')
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	@php(do_action('get_header'))
+	@php(wp_head())
+</head>
 
-  <main id="main" class="main">
-    @yield('content')
-  </main>
+<body @php(body_class())>
+	@php(wp_body_open())
 
-  @hasSection('sidebar')
-    <aside class="sidebar">
-      @yield('sidebar')
-    </aside>
-  @endif
+	<div id="app">
+		<a class="sr-only focus:not-sr-only" href="#main">
+			{{ __('Skip to content', 'sage') }}
+		</a>
 
-@include('sections.footer')
+		@include('sections.header')
+
+		<main id="main" class="main">
+			@yield('content')
+		</main>
+
+		@hasSection('sidebar')
+			<aside class="sidebar">
+				@yield('sidebar')
+			</aside>
+		@endif
+
+		@include('sections.footer')
+	</div>
+
+	@php(do_action('get_footer'))
+	@php(wp_footer())
+</body>
+
+</html>
